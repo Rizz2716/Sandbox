591918d1fbc5a92b19dae9747cc46375b3268710
Rizz2716 <41773993+Rizz2716@users.noreply.github.com>
2018-11-20 13:41:30 +0800

    try this

--- oddname.py
+++ oddname.py
@@ -1,1 +1,8 @@
 """"input a name and print altnertive character"""""
+def main():
+    name=input("enter your name")
+    while len(name)<1:
+        name=input("Enter longer name")
+        print(name[::2])
+
+main()
